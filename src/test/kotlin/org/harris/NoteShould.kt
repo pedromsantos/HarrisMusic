b0369e54d93438b49fb4d9b6e2e88fe277c1bc02
Pedro Santos <pedro.santos.personal@gmail.com>
2020-06-07 20:05:22 +0200

    Remove parameterized tests on Note tests

--- src/test/kotlin/org/harris/NoteShould.kt
+++ src/test/kotlin/org/harris/NoteShould.kt
@@ -1,72 +1,51 @@
 package org.harris
 
-import junit.framework.Assert.assertEquals
-import junitparams.JUnitParamsRunner
 import junitparams.Parameters
 import org.harris.Note.*
 import org.junit.Test
-import org.junit.runner.RunWith
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
 
-
-@RunWith(JUnitParamsRunner::class)
 class NoteShould {
     @Test
-    @Parameters(method = "sharpingNotesValues")
-    fun `transpose note by a half step up when sharped`(note: Note, sharpedNote:Note) {
-        val to = note.sharp()
-
-        assertEquals(sharpedNote, to)
+    fun `transpose note by a half step up when sharped`() {
+        assertThat(C.sharp(), equalTo(CSharp))
+        assertThat(CSharp.sharp(), equalTo(D))
+        assertThat(DFlat.sharp(), equalTo(D))
+        assertThat(D.sharp(), equalTo(DSharp))
+        assertThat(DSharp.sharp(), equalTo(E))
+        assertThat(EFlat.sharp(), equalTo(E))
+        assertThat(E.sharp(), equalTo(F))
+        assertThat(F.sharp(), equalTo(FSharp))
+        assertThat(FSharp.sharp(), equalTo(G))
+        assertThat(GFlat.sharp(), equalTo(G))
+        assertThat(G.sharp(), equalTo(GSharp))
+        assertThat(GSharp.sharp(), equalTo(A))
+        assertThat(AFlat.sharp(), equalTo(A))
+        assertThat(A.sharp(), equalTo(ASharp))
+        assertThat(ASharp.sharp(), equalTo(B))
+        assertThat(BFlat.sharp(), equalTo(B))
+        assertThat(B.sharp(), equalTo(C))
     }
 
     @Test
-    @Parameters(method = "flateningNotesValues")
-    fun `transpose note by a half step down when flated`(note: Note, flatedNote:Note) {
-        val to = note.flat()
-
-        assertEquals(flatedNote, to)
-    }
-
-    private fun sharpingNotesValues(): Array<Any>? {
-        return arrayOf(
-            arrayOf<Any>(C, CSharp),
-            arrayOf<Any>(CSharp, D),
-            arrayOf<Any>(DFlat, D),
-            arrayOf<Any>(D, DSharp),
-            arrayOf<Any>(DSharp, E),
-            arrayOf<Any>(EFlat, E),
-            arrayOf<Any>(E, F),
-            arrayOf<Any>(F, FSharp),
-            arrayOf<Any>(FSharp, G),
-            arrayOf<Any>(GFlat, G),
-            arrayOf<Any>(G, GSharp),
-            arrayOf<Any>(GSharp, A),
-            arrayOf<Any>(AFlat, A),
-            arrayOf<Any>(A, ASharp),
-            arrayOf<Any>(ASharp, B),
-            arrayOf<Any>(BFlat, B),
-            arrayOf<Any>(B, C)
-        )
-    }
-
-    private fun flateningNotesValues(): Array<Any>? {
-        return arrayOf(
-            arrayOf<Any>(C, B),
-            arrayOf<Any>(CSharp, C),
-            arrayOf<Any>(DFlat, C),
-            arrayOf<Any>(D, DFlat),
-            arrayOf<Any>(DSharp, D),
-            arrayOf<Any>(EFlat, D),
-            arrayOf<Any>(E, EFlat),
-            arrayOf<Any>(F, E),
-            arrayOf<Any>(FSharp, F),
-            arrayOf<Any>(GFlat, F),
-            arrayOf<Any>(G, GFlat),
-            arrayOf<Any>(GSharp, G),
-            arrayOf<Any>(AFlat, G),
-            arrayOf<Any>(A, AFlat),
-            arrayOf<Any>(ASharp, A),
-            arrayOf<Any>(BFlat, A),
-            arrayOf<Any>(B, BFlat)
-        )
+    fun `transpose note by a half step down when flated`() {
+        assertThat(C.flat(), equalTo(B))
+        assertThat(CSharp.flat(), equalTo(C))
+        assertThat(DFlat.flat(), equalTo(C))
+        assertThat(D.flat(), equalTo(DFlat))
+        assertThat(DSharp.flat(), equalTo(D))
+        assertThat(EFlat.flat(), equalTo(D))
+        assertThat(E.flat(), equalTo(EFlat))
+        assertThat(F.flat(), equalTo(E))
+        assertThat(FSharp.flat(), equalTo(F))
+        assertThat(GFlat.flat(), equalTo(F))
+        assertThat(G.flat(), equalTo(GFlat))
+        assertThat(GSharp.flat(), equalTo(G))
+        assertThat(AFlat.flat(), equalTo(G))
+        assertThat(A.flat(), equalTo(AFlat))
+        assertThat(ASharp.flat(), equalTo(A))
+        assertThat(BFlat.flat(), equalTo(A))
+        assertThat(B.flat(), equalTo(BFlat))
     }
 }
