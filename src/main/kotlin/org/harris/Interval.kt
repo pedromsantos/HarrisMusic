d44656e9725e48ca0e76688e369035a04b441851
Pedro Santos <pedro.santos.personal@gmail.com>
2020-06-09 23:55:43 +0200

    Simplify interval transpose method

--- src/main/kotlin/org/harris/Interval.kt
+++ src/main/kotlin/org/harris/Interval.kt
@@ -2,63 +2,38 @@ package org.harris
 
 import org.harris.Note.*
 
-enum class Interval(private val intervalName: String, private val abreviature: String, private val distance: Int, private val naturalDistance: Int) {
-    Unison("Unisson", "U", 0, C.naturalDistance(C)),
-    AugmentedUnison("AugmentedUnison", "A1", 1, C.naturalDistance(CSharp)),
-    MinorSecond("MinorSecond", "m2", 1, C.naturalDistance(DFlat)),
-    MajorSecond("MajorSecond", "M2", 2, C.naturalDistance(D)),
-    AugmentedSecond("AugmentedSecond", "A2", 3, C.naturalDistance(DSharp)),
-    MinorThird("MinorThird", "m3", 3, C.naturalDistance(EFlat)),
-    MajorThird("MajorThird", "M3", 4, C.naturalDistance(E)),
-    PerfectFourth("PerfectFourth", "P4", 5, C.naturalDistance(F)),
-    AugmentedFourth("AugmentedFourth", "A4", 6, C.naturalDistance(FSharp)),
-    DiminishedFifth("DiminishedFifth", "d5", 6, C.naturalDistance(GFlat)),
-    Tritone("DiminishedFifth", "d5", 6, C.naturalDistance(GFlat)),
-    PerfectFifth("PerfectFifth", "P5", 7, C.naturalDistance(G)),
-    AugmentedFifth("AugmentedFifth", "A5", 8, C.naturalDistance(GSharp)),
-    MinorSixth("MinorSixth", "m6", 8, C.naturalDistance(AFlat)),
-    MajorSixth("MajorSixth", "M6", 9, C.naturalDistance(A)),
-    DiminishedSeventh("DiminishedSeventh", "d7", 9, C.naturalDistance(A)),
-    MinorSeventh("MinorSeventh", "m7", 10, C.naturalDistance(BFlat)),
-    MajorSeventh("MajorSeventh", "M7", 11, C.naturalDistance(B)),
-    PerfectOctave("PerfectOctave", "PO", 12, C.naturalDistance(C)),
-    MinorNinth("MinorNinth", "m9", 13, C.naturalDistance(DFlat)),
-    MajorNinth("MajorNinth", "M9", 14, C.naturalDistance(D)),
-    AugmentedNinth("AugmentedNinth", "A9", 15, C.naturalDistance(DSharp)),
-    PerfectEleventh("PerfectEleventh", "P11", 17, C.naturalDistance(F)),
-    AugmentedEleventh("AugmentedEleventh", "A11", 18, C.naturalDistance(FSharp)),
-    MinorThirteenth("MinorThirteenth", "m13", 20, C.naturalDistance(AFlat)),
-    MajorThirteenth("MajorThirteenth", "M13", 21, C.naturalDistance(A));
+enum class Interval(private val intervalName: String, private val abreviature: String, private val distance: Int, private val naturalDistance: Int, private val transposeValue: Int) {
+    Unison("Unisson", "U", 0, C.naturalDistance(C), 0),
+    AugmentedUnison("AugmentedUnison", "A1", 1, C.naturalDistance(CSharp), 1),
+    MinorSecond("MinorSecond", "m2", 1, C.naturalDistance(DFlat), 2),
+    MajorSecond("MajorSecond", "M2", 2, C.naturalDistance(D), 3),
+    AugmentedSecond("AugmentedSecond", "A2", 3, C.naturalDistance(DSharp),4),
+    MinorThird("MinorThird", "m3", 3, C.naturalDistance(EFlat), 5),
+    MajorThird("MajorThird", "M3", 4, C.naturalDistance(E), 6),
+    PerfectFourth("PerfectFourth", "P4", 5, C.naturalDistance(F), 7),
+    AugmentedFourth("AugmentedFourth", "A4", 6, C.naturalDistance(FSharp), 8),
+    DiminishedFifth("DiminishedFifth", "d5", 6, C.naturalDistance(GFlat), 9),
+    Tritone("DiminishedFifth", "d5", 6, C.naturalDistance(GFlat), 9),
+    PerfectFifth("PerfectFifth", "P5", 7, C.naturalDistance(G), 10),
+    AugmentedFifth("AugmentedFifth", "A5", 8, C.naturalDistance(GSharp), 11),
+    MinorSixth("MinorSixth", "m6", 8, C.naturalDistance(AFlat), 12),
+    MajorSixth("MajorSixth", "M6", 9, C.naturalDistance(A), 13),
+    DiminishedSeventh("DiminishedSeventh", "d7", 9, C.naturalDistance(A), 14),
+    MinorSeventh("MinorSeventh", "m7", 10, C.naturalDistance(BFlat), 15),
+    MajorSeventh("MajorSeventh", "M7", 11, C.naturalDistance(B), 16),
+    PerfectOctave("PerfectOctave", "PO", 12, C.naturalDistance(C), 17),
+    MinorNinth("MinorNinth", "m9", 13, C.naturalDistance(DFlat), 18),
+    MajorNinth("MajorNinth", "M9", 14, C.naturalDistance(D), 19),
+    AugmentedNinth("AugmentedNinth", "A9", 15, C.naturalDistance(DSharp), 20),
+    PerfectEleventh("PerfectEleventh", "P11", 17, C.naturalDistance(F), 21),
+    AugmentedEleventh("AugmentedEleventh", "A11", 18, C.naturalDistance(FSharp), 22),
+    MinorThirteenth("MinorThirteenth", "m13", 20, C.naturalDistance(AFlat), 23),
+    MajorThirteenth("MajorThirteenth", "M13", 21, C.naturalDistance(A), 24);
 
     fun naturalDistance() =  naturalDistance
 
     fun transpose(from: Note) : Note {
-        return when (this) {
-            MinorSecond,
-            MajorSecond,
-            AugmentedSecond -> from.transpose(this.distance + 1)
-            PerfectFourth,
-            AugmentedFourth,
-            MajorThird,
-            MinorThird -> from.transpose(this.distance + 2)
-            AugmentedFifth,
-            PerfectFifth,
-            Tritone,
-            DiminishedFifth -> from.transpose(this.distance + 3)
-            MajorSixth,
-            MinorSixth -> from.transpose(this.distance + 4)
-            AugmentedNinth,
-            PerfectEleventh,
-            AugmentedEleventh,
-            MajorThirteenth,
-            MinorNinth,
-            MajorNinth,
-            MinorThirteenth,
-            DiminishedSeventh,
-            MajorSeventh,
-            MinorSeventh -> from.transpose(this.distance + 5)
-            else -> from.transpose(this.distance)
-        }
+        return from.transpose(this.transposeValue)
     }
 
     companion object {
