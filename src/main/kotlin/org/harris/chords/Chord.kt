b3a18958c87c42c2876052a2c056952b4f58c5e2
Pedro Santos <pedromsantos@users.noreply.github.com>
2025-08-22 10:16:47 +0200

    Update Chord.kt

--- src/main/kotlin/org/harris/chords/Chord.kt
+++ src/main/kotlin/org/harris/chords/Chord.kt
@@ -43,21 +43,13 @@ abstract class BaseChord : Chord {
 
     override fun pitches(): Array<Pitch> = pitches.notes()
 
-    override fun bass(): Pitch {
-        return pitches.bass().pitch
-    }
+    override fun bass(): Pitch = return pitches.bass().pitch
 
-    override fun lead(): Pitch {
-        return pitches.lead().pitch
-    }
+    override fun lead(): Pitch = return pitches.lead().pitch
 
-    override fun name(): String {
-        return root.pitch.name + pattern.name
-    }
+    override fun name(): String = return root.pitch.name + pattern.name
 
-    override fun pitchForFunction(function: ChordFunction): Pitch {
-        return pitches.pitchForFunction(function).pitch
-    }
+    override fun pitchForFunction(function: ChordFunction): Pitch = return pitches.pitchForFunction(function).pitch
 
     override fun drop2(): Chord {
         if (pitches().size == 4) {
